9183f17e5f9e8ff8ea3fc33c9cd4710ce8e6ed0e
KHUSHNOOD799KODING <collageacc799ount@gmail.com>
2023-12-16 01:13:40 +0530

    web devolopment upload

---
+++ script.js
@@ -0,0 +1,187 @@
+
+
+function theme() {
+    let indark = document.getElementById('indark');
+    let circle = document.getElementById('circle');
+    let body = document.querySelector('body');
+    let a = document.getElementById('a');
+    let b = document.getElementById('b');
+    let c = document.getElementById('c');
+    let d = document.getElementById('d');
+    let e = document.getElementById('e');
+    let f = document.getElementById('f');
+    let g = document.getElementById('g');
+    let h = document.getElementById('h');
+    let i = document.getElementById('i');
+    let drk = document.getElementById("drk");
+
+    if (indark.style.backgroundColor != "black") {
+        indark.style.backgroundColor = "black"
+        
+    }
+    else{
+        indark.style.backgroundColor = "white"
+    };
+
+    if (circle.style.position != "relative") {
+        circle.style.position = "relative"
+    }
+    else{
+        circle.style.position = "static"
+    };
+
+    if (circle.style.transform != "translateX(35px)") {
+        circle.style.transform = "translateX(35px)"
+    }
+    else{
+        circle.style.transform = "translateX(0)"
+    };
+    if (body.style.backgroundColor != "black") {
+         body.style.backgroundColor = "black"
+    }
+    else{
+        body.style.backgroundColor= "white"
+    };
+    if (drk.innerHTML != "LightMode") {
+        drk.innerHTML = "LightMode"
+    }
+    else{
+        drk.innerHTML = "DarkMode"
+    };
+
+    if (drk.style.color != "white") {
+        drk.style.color = "white"
+    }
+    else{
+        drk.style.color = "black"
+    };
+
+
+    if (a.style.backgroundColor != "black") {
+        a.style.backgroundColor = "black"
+    }
+    else{
+        a.style.backgroundColor = "white"
+    };
+
+    if (b.style.backgroundColor != "black") {
+        b.style.backgroundColor = "black"
+    }
+    else{
+        b.style.backgroundColor = "white"
+    }
+    if (c.style.backgroundColor != "black") {
+        c.style.backgroundColor = "black"
+    }
+    else{
+        c.style.backgroundColor = "white"
+    }
+    if (g.style.backgroundColor != "black") {
+        g.style.backgroundColor = "black"
+    }
+    else{
+        g.style.backgroundColor = "white"
+    }
+    if (h.style.backgroundColor != "black") {
+        h.style.backgroundColor = "black"
+    }
+    else{
+        h.style.backgroundColor = "white"
+    }
+    if (i.style.backgroundColor != "black") {
+        i.style.backgroundColor = "black"
+    }
+    else{
+        i.style.backgroundColor = "white"
+    }
+
+    if (g.style.color != "white") {
+        g.style.color = "white"
+    }
+    else{
+        g.style.color = "black"
+    };
+    if (h.style.color != "white") {
+        h.style.color = "white"
+    }
+    else{
+        h.style.color = "black"
+    };
+    if (i.style.color != "white") {
+        i.style.color = "white"
+    }
+    else{
+        i.style.color = "black"
+    };
+
+    if (a.style.color != "white") {
+        a.style.color = "white"
+    }
+    else{
+        a.style.color = "black"
+    };
+
+    if (b.style.color != "white") {
+        b.style.color = "white"
+    }
+    else{
+        b.style.color = "black"
+    };
+
+    if (c.style.color != "white") {
+        c.style.color = "white"
+    }
+    else{
+        c.style.color = "black"
+    };
+    
+    
+    if (d.style.backgroundColor != "black") {
+        d.style.backgroundColor = "black"
+    }
+    else{
+        d.style.backgroundColor = "white"
+    };
+
+    if (e.style.backgroundColor != "black") {
+        e.style.backgroundColor = "black"
+    }
+    else{
+        e.style.backgroundColor = "white"
+    };
+
+    if (f.style.backgroundColor != "black") {
+        f.style.backgroundColor = "black"
+    }
+    else{
+        f.style.backgroundColor = "white"
+    };
+
+    if (d.style.color != "white") {
+        d.style.color = "white"
+    }
+    else{
+        d.style.color = "black"
+    };
+    
+    if (e.style.color != "rgb(204, 0, 255)") {
+        e.style.color = "rgb(204, 0, 255)"
+    }
+    else{
+        e.style.color = "rgb(38, 38, 250)"
+    };
+
+    if (f.style.color != "yellow") {
+        f.style.color = "yellow"
+    }
+    else{
+        f.style.color = "rgb(204, 0, 255)"
+    };
+
+
+    
+}
+
+
+
+
